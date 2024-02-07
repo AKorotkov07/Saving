6fef966d42eb21498b5e6e36e1fa6c4571ff53a1
AKorotkov07 <153372700+AKorotkov07@users.noreply.github.com>
2024-02-07 17:02:03 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,9 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
@@ -14,18 +13,17 @@ public class Main {
 		GameProgress progress2 = new GameProgress(90, 2, 3, 100.0);
 		GameProgress progress3 = new GameProgress(80, 1, 1, 50.0);
 
-		saveGame("C:\\Users\\Alex\\Desktop\\Games2\\savegames\\save1.dat", progress1);
-		saveGame("C:\\Users\\Alex\\Desktop\\Games2\\savegames\\save2.dat", progress2);
-		saveGame("C:\\Users\\Alex\\Desktop\\Games2\\savegames\\save3.dat", progress3);
+		saveGame("C:/Users/Alex/Desktop/Games2/savegames/save1.dat", progress1);
+		saveGame("C:/Users/Alex/Desktop/Games2/savegames/save2.dat", progress2);
+		saveGame("C:/Users/Alex/Desktop/Games2/savegames/save3.dat", progress3);
 
 		List<String> files = new ArrayList<>();
-		files.add("C:\\Users\\Alex\\Desktop\\Games2\\savegames\\save1.dat");
-		files.add("C:\\Users\\Alex\\Desktop\\Games2\\savegames\\save2.dat");
-		files.add("C:\\Users\\Alex\\Desktop\\Games2\\savegames\\save3.dat");
+		files.add("C:/Users/Alex/Desktop/Games2/savegames/save1.dat");
+		files.add("C:/Users/Alex/Desktop/Games2/savegames/save2.dat");
+		files.add("C:/Users/Alex/Desktop/Games2/savegames/save3.dat");
 
-		zipFiles("C:\\Users\\Alex\\Desktop\\Games2\\savegames\\zip.zip", files);
+		zipFiles("C:/Users/Alex/Desktop/Games2/savegames/zip.zip", files);
 		deleteFiles(files);
-		extractFilesFromArchive("C:\\Users\\Alex\\Desktop\\Games2\\savegames\\zip.zip", "C:\\Users\\Alex\\Desktop\\Games2\\savegames\\extracted");
 
 	}
 
@@ -43,7 +41,7 @@ public class Main {
 			 ZipOutputStream zos = new ZipOutputStream(fos)) {
 			for (String filePath : files) {
 				try (FileInputStream fis = new FileInputStream(filePath)) {
-					ZipEntry zipEntry = new ZipEntry(filePath);
+					ZipEntry zipEntry = new ZipEntry(filePath.substring(filePath.lastIndexOf('/') + 1));
 					zos.putNextEntry(zipEntry);
 					byte[] buffer = new byte[fis.available()];
 					fis.read(buffer);
@@ -59,27 +57,26 @@ public class Main {
 		}
 	}
 
-	public static void extractFilesFromArchive(String zipFilePath, String extractPath) {
-		try (ZipFile zipFile = new ZipFile(zipFilePath)) {
-			Enumeration<? extends ZipEntry> entries = zipFile.entries();
-			while (entries.hasMoreElements()) {
-				ZipEntry entry = entries.nextElement();
+	public static void extractFilesFromArchive(String zipPath, String extractPath) {
+		try (ZipInputStream zis = new ZipInputStream(new FileInputStream(zipPath))) {
+			ZipEntry entry;
+			while ((entry = zis.getNextEntry()) != null) {
 				String filePath = extractPath + File.separator + entry.getName();
-				try (InputStream is = zipFile.getInputStream(entry);
-					 FileOutputStream fos = new FileOutputStream(filePath)) {
-					byte[] buffer = new byte[1024];
-					int length;
-					while ((length = is.read(buffer)) > 0) {
-						fos.write(buffer, 0, length);
+				try (FileOutputStream fos = new FileOutputStream(filePath)) {
+					for (int c = zis.read(); c != -1; c = zis.read()) {
+						fos.write(c);
 					}
-				} catch (IOException e) {
-					e.printStackTrace();
+					fos.flush();
 				}
+				zis.closeEntry();
 			}
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
 		} catch (IOException e) {
-			e.printStackTrace();
+			throw new RuntimeException(e);
 		}
 	}
+
 
 	public static void deleteFiles(List<String> files) {
 		for (String filePath : files) {
